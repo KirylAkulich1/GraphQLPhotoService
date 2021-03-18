fc1dd62adf624ca3a69b30c24e8b1cb5978f9e3d
kirillakulich <kirillakulich0@gmail.com>
2021-03-18 14:55:30 +0300

    m:m relation added

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const { ApolloServer } = require('apollo-server')
 
 const typeDefs = `
+        enum PhotoCategory {
+            SELFIE
+            PORTRAIT
+            ACTION
+            LANDSCAPE
+            GRAPHIC
+        }
+
         type Photo{
             id: ID!
             url: String!
             name: String!
+            description: String
+            category: PhotoCategory!
+            postedBy: User!
+            taggedUsers: [User!]!
+        }
+
+        type User{
+            githubLogin: ID!
+            name: String
+            avatar: String
+            postedPhotos: [Photo!]!
+            inPhotos: [Photo!]!
+        }
+
+        input PostPhotoInput {
+            name: String!
+            category: PhotoCategory=PORTRAIT
             description: String
         }
 
@@ -14,14 +39,47 @@ const typeDefs = `
         }
 
         type Mutation{
-            postPhoto(name: String! description: String): Photo!
+            postPhoto(input: PostPhotoInput): Photo!
         }
         `
 
 
 var _id = 0
-var photos=[]
 
+var photos = [
+    {
+    "id": "1",
+    "name": "Dropping the Heart Chute",
+    "description": "The heart chute is one of my favorite chutes",
+    "category": "ACTION",
+    "githubUser": "gPlake"
+    },
+    {
+    "id": "2",
+    "name": "Enjoying the sunshine",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt"
+    },
+    {
+    id: "3",
+    "name": "Gunbarrel 25",
+    "description": "25 laps on gunbarrel today",
+    "category": "LANDSCAPE",
+    "githubUser": "sSchmidt"
+    }
+    ]
+var users = [
+{"githubLogin": "mHattrup", "name": "Mike Hattrup" },
+{"githubLogin": "gPlake", "name": "Glen Plake" },
+{"githubLogin": "sSchmidt", "name": "Scot Schmidt" }]
+
+var tags = [
+    { "photoID": "1","userID": "gPlake" },
+    { "photoID": "2","userID": "sSchmidt" },
+    { "photoID": "2","userID": "mHattrup" },
+    { "photoID": "2","userID": "gPlake" },
+
+]
 const resolvers= {
     Query: {
         totalPhotos: () => photos.length,
@@ -33,13 +91,36 @@ const resolvers= {
 
             var newPhoto={
                 id: _id++,
-                ...args 
+                ...args.input
             }
             photos.push(newPhoto)
             return newPhoto
-            
+
         }
+    },
+
+    Photo:{
+        url: parent => `http://site.com/img/${parent.id}.jpg`,
+        postedBy: parent => {
+            return users.find(u => u.githubLogin === parent.githubUser)
+        },
+        taggedUsers: parent => tags
+        .filter( tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map( userID => users.find(u => u.githubLogin === userID))
+    },
+    User: {
+        postedPhotos: parent =>{
+            return photos.filter(p=>p.githubUser ===
+                                    parent.githubLogin)
+        },
+
+        inPhotos: parent => tags
+                    .filter(tag => tag.userID === parent.id)
+                    .map(tag => tag.photoID)
+                    .map(photoID => photos.find(p => p.id === photoID))
     }
+
 }
 
 const server = new ApolloServer({
