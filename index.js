0d557264c7be267e1a8a332c221d42e934d19290
kirillakulich <kirillakulich0@gmail.com>
2021-03-18 15:35:00 +0300

    express added

--- index.js
+++ index.js
@@ -1,134 +1,26 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
 
-const typeDefs = `
-        enum PhotoCategory {
-            SELFIE
-            PORTRAIT
-            ACTION
-            LANDSCAPE
-            GRAPHIC
-        }
+const expressPlayground = require('graphql-playground-middleware-express').default
+const { readFileSync } = require('fs')
 
-        type Photo{
-            id: ID!
-            url: String!
-            name: String!
-            description: String
-            category: PhotoCategory!
-            postedBy: User!
-            taggedUsers: [User!]!
-        }
-
-        type User{
-            githubLogin: ID!
-            name: String
-            avatar: String
-            postedPhotos: [Photo!]!
-            inPhotos: [Photo!]!
-        }
-
-        input PostPhotoInput {
-            name: String!
-            category: PhotoCategory=PORTRAIT
-            description: String
-        }
-
-        type Query{
-            totalPhotos: Int!
-            allPhotos: [Photo!]!
-        }
-
-        type Mutation{
-            postPhoto(input: PostPhotoInput): Photo!
-        }
-        `
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+const resolvers = require('./resolvers')
 
 
-var _id = 0
+var app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
 
-var photos = [
-    {
-    "id": "1",
-    "name": "Dropping the Heart Chute",
-    "description": "The heart chute is one of my favorite chutes",
-    "category": "ACTION",
-    "githubUser": "gPlake"
-    },
-    {
-    "id": "2",
-    "name": "Enjoying the sunshine",
-    "category": "SELFIE",
-    "githubUser": "sSchmidt"
-    },
-    {
-    id: "3",
-    "name": "Gunbarrel 25",
-    "description": "25 laps on gunbarrel today",
-    "category": "LANDSCAPE",
-    "githubUser": "sSchmidt"
-    }
-    ]
-var users = [
-{"githubLogin": "mHattrup", "name": "Mike Hattrup" },
-{"githubLogin": "gPlake", "name": "Glen Plake" },
-{"githubLogin": "sSchmidt", "name": "Scot Schmidt" }]
+server.applyMiddleware({ app })
 
-var tags = [
-    { "photoID": "1","userID": "gPlake" },
-    { "photoID": "2","userID": "sSchmidt" },
-    { "photoID": "2","userID": "mHattrup" },
-    { "photoID": "2","userID": "gPlake" },
+app.get('/', (req, res) => res.end('Welcome to the PhotoShareAPI'))
+app.get('/playground', expressPlayground({ endpoint:
+    '/graphql' }))
 
-]
-const resolvers= {
-    Query: {
-        totalPhotos: () => photos.length,
-        allPhotos: () => photos
-    },
 
-    Mutation: {
-        postPhoto(parent,args){
 
-            var newPhoto={
-                id: _id++,
-                ...args.input
-            }
-            photos.push(newPhoto)
-            return newPhoto
 
-        }
-    },
-
-    Photo:{
-        url: parent => `http://site.com/img/${parent.id}.jpg`,
-        postedBy: parent => {
-            return users.find(u => u.githubLogin === parent.githubUser)
-        },
-        taggedUsers: parent => tags
-        .filter( tag => tag.photoID === parent.id)
-        .map(tag => tag.userID)
-        .map( userID => users.find(u => u.githubLogin === userID))
-    },
-    User: {
-        postedPhotos: parent =>{
-            return photos.filter(p=>p.githubUser ===
-                                    parent.githubLogin)
-        },
-
-        inPhotos: parent => tags
-                    .filter(tag => tag.userID === parent.id)
-                    .map(tag => tag.photoID)
-                    .map(photoID => photos.find(p => p.id === photoID))
-    }
-
-}
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-}) 
-
-server
-    .listen()
-    .then(({url}) => console.log(`GraphQL Service running 
-        on ${url}`))
+app.listen({ port: 4000 }, () =>
+    console.log(`GraphQL Server running @ http://
+    localhost:4000${server.graphqlPath}`)
+)
